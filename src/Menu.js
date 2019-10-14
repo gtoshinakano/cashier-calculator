9050fa8ced82ea9ddf2fcdb47893a15127952160
Gabriel Toshinori Nakano <gtnakano@daeesp.gov>
2019-10-14 15:24:22 -0300

    Started App Structure with tabs menu

---
+++ src/Menu.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import SettingsIcon from '@material-ui/icons/SettingsApplications';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+export default function Menu(props) {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    props.changeTab(value)
+  };
+
+  return (
+    <Paper square className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="fullWidth"
+        indicatorColor="primary"
+        textColor="primary"
+        aria-label="icon tabs example"
+      >
+        <Tab icon={<ListAltIcon />} aria-label="phone" label="CALCULADORA" />
+        <Tab icon={<SettingsIcon />} aria-label="favorite" label="CADASTRAR" />
+      </Tabs>
+    </Paper>
+  );
+}
